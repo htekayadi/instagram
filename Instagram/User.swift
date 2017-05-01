154f5bc6683ee58e0acfd8b573cca8d41321fbe2
Hendrik Tekayadi <hendrik@popsical.com>
2017-05-01 10:09:18 +0800

    Add User & Post model

---
+++ Instagram/User.swift
@@ -0,0 +1,18 @@
+//
+//  User.swift
+//  Instagram
+//
+//  Created by Popsical on 1/5/17.
+//  Copyright © 2017 htekayadi. All rights reserved.
+//
+
+import UIKit
+
+struct User
+{
+    var username: String?
+    var profileImage: UIImage?
+}
+
+
+
